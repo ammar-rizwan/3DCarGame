ddf114020aa3f7968bdbb89fdb590933a5bf04ca
ammar-rizwan <m.ammarizwan@gmail.com>
2020-06-01 20:17:08 +0500

    Car Select Menu

---
+++ Assets/Animation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    [SerializeField] Vector3 finalPosition;
+    Vector3 initialPosition;
+    void Awake()
+    {
+        initialPosition = transform.position;
+    }
+    // Start is called before the first frame update
+    void OnDisable()
+    {
+        transform.position = initialPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, finalPosition, 0.1f);
+    }
+}
---
+++ Assets/CarSelection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarSelection : MonoBehaviour
+{
+    [SerializeField] Button next;
+    [SerializeField] Button previous;
+    private int currentCar;
+    private void Awake()
+    {
+        SelectCar(0);
+    }
+    private void SelectCar(int _index)
+    {
+
+        previous.interactable = (_index != 0);
+        next.interactable = (_index != transform.childCount-1);
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(i ==_index);
+        }
+        
+    }
+
+    // Update is called once per frame
+    public void ChangeCar(int _change)
+    {
+        Debug.Log("ammar");
+        currentCar += _change;
+        SelectCar(currentCar);
+        
+    }
+}
